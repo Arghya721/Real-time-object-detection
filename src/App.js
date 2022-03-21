20569985421e54fdbecb2dfa3cea16c3c6138393
Arghya721 <imarghyadeep@gmail.com>
2022-03-21 18:01:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import Video from './Components/video';
+import { useSpeechSynthesis } from 'react-speech-kit';
+
+import {useEffect, useState} from 'react';
+import SpeechRecognition, {
+  useSpeechRecognition
+} from "react-speech-recognition";
+
 
 function App() {
+  const [video, setVideo] = useState(0);
+
+  const [value, setValue] = useState('');
+  const { speak } = useSpeechSynthesis();
+
+  SpeechRecognition.startListening({ continuous: true , language:'en-IN' });
+  
+  const commands = [
+    {
+      command: ["Start", "Start the process"],
+      callback: () => {
+        speak({text : "Starting the process"});
+        setVideo(1);
+        SpeechRecognition.startListening({ continuous: true , language:'en-IN' });
+      }
+    },
+    {
+      command: ["Stop", "Stop the process"],
+      callback: () => {
+        speak({text : "Stopping the process"});
+        setVideo(0);
+        SpeechRecognition.startListening({ continuous: true , language:'en-IN' });
+      }
+    }
+  ];
+
+  const { transcript, resetTranscript , listening } = useSpeechRecognition({ commands });
+  
+
+  // if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+  //   return null;
+  // }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <button onClick={()=>{
+        {video === 0 ? setVideo(1) : setVideo(0)}
+      }}>
+        {video === 1 ? "Stop Detection" :  "Start Detection"}
+      </button>
+      {video === 1 ? <Video /> : ""}
+
+      <div>
+      <p>Microphone: {listening ? 'on' : 'off'}</p>
+      <button onClick={SpeechRecognition.startListening}>Start</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
+    </div>
+      
     </div>
   );
 }
